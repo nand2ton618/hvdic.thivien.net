d3f9a8dfa398a54439d17847c328496f462574a8
nand2ton618 <nand2ton618@gmail.com>
2023-02-23 13:54:09 +0700

    hmm

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,23 @@
 import ankiAxios from './ankiAxios'
 import crawl, { CrawlData } from './crawl'
-import { getPosition } from './rateLimit'
+import { getPosition, savePosition } from './rateLimit'
+import config from 'config'
 
-type CardAction = {
+export type CardAction = {
   action: string
   version: 6
   params: any
 }
-
-const DECKNAME = 'Kanji'
 
 const getNotes = async (): Promise<any[]> => {
   const findNotes: CardAction = {
     action: 'findNotes',
     version: 6,
     params: {
-      query: DECKNAME,
+      query: 'deck:current',
     },
   }
+
   const res = await ankiAxios.post('/', findNotes)
 
   const notesInfo: CardAction = {
@@ -58,9 +58,9 @@ const updateNote = async (noteId: number, han: string) => {
 }
 
 const run = async () => {
-  const notes = await getNotes()
-
   const position = getPosition()
+  console.log(position)
+  const notes = await getNotes()
 
   /* can't pass rate-limit */
 
@@ -72,13 +72,50 @@ const run = async () => {
   // }
 
   // use crontab instead
-  for (let i = position; i < position + 100; i++) {
-    const note = notes[i]
-    const crawledData = await crawl(note.fields.Kanji.value)
+  // let i = position
+  // while (i < position + 10) {
+  //   const note = notes[i]
+  //   const crawledData = await crawl(note.fields.Kanji.value)
 
-    console.log('Updating ', note.noteId, note.fields.Kanji.value, crawledData)
+  //   console.log('Updating ', note.noteId, note.fields.Kanji.value, crawledData)
+  //   await updateNote(note.noteId, crawledData)
+  //   console.log('Updated  ', note.noteId, note.fields.Kanji.value)
+  //   i++
+  // }
+  // savePosition(i)
+
+  // use crontab instead
+  let i = position
+  while (i < position + config.get<number>('KANJI_NUM')) {
+    const note = notes[i]
+
+    const crawledData = await crawl(note.fields.kanji.value)
+
+    console.log(
+      'Updating ',
+      i,
+      note.noteId,
+      note.fields.kanji.value,
+      crawledData
+    )
     await updateNote(note.noteId, crawledData)
-    console.log('Updated  ', note.noteId, note.fields.Kanji.value)
+    console.log('Updated ', i, note.noteId, note.fields.kanji.value)
+    i++
   }
+  savePosition(i)
 }
+
+const deckNames = async () => {
+  const deckName: CardAction = {
+    action: 'deckNames',
+    version: 6,
+    params: {},
+  }
+  const res = await ankiAxios.post('/', deckName)
+  console.log(res.data.result)
+  console.log(res.data.result[33])
+  return res.data.result
+}
+
+// deckNames()
 run()
